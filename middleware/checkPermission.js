cf33c3572aee7a3e54db2e73babf12aab8fa30b9
sragu2000 <ragurajsivanantham@gmail.com>
2023-08-06 12:06:37 +0530

    Profile View and Todo API

--- middleware/checkPermission.js
+++ middleware/checkPermission.js
@@ -5,19 +5,24 @@ const auth = (userRoles) => {
   return (req, res, next) => {
     const token = req.header("token");
     if (!token) {
-      return res.status(401).json({ message: 'Unauthorized', status: false });
+      return res.status(401).json(
+        // {
+        //   message: "Unauthorized",
+        //   status: false,
+        // }
+      );
     } else {
       jwt.verify(token, process.env.JWT_SECRET, function (err, decoded) {
         if (err) {
-          // return when 
-          return res.status(401).json({ message: "Unauthorized 2", status: false });
+          return res.status(401).json(
+            // {
+            //   message: "Unauthorized",
+            //   status: false
+            // }
+          );
         } else {
-          // decoded loggedin user data will be available inside the decoded object
-          // the user roles that can access next end point list is available in userRoles array
-          // if decoded.userData.userRole is available inside the userRoles array, 
           if (userRoles.includes(decoded.userData.userRole)) {
-            // in next end point we can access this loggedInUser data
-            req.loggedInUser = decoded.userData;
+            req.currentUser = decoded.userData;
             next()
           }
         }
--- userRoles/userRoles.js
+++ userRoles/userRoles.js
@@ -1,8 +1,17 @@
 const userRoles = {
-    hiredEmployee : "Hired Employee", 
-    contentCreator : "Content Creator",
-    supervisor : "Supervisor",
-    systemAdmin : "System Admin",
-    superAdmin : "Super Admin" 
+    superAdmin: "Super Admin",
+    operationTeam: "Operation Management",
+    resourceManager: "Resource Management",
+    projectManager: "Project Manager",
+    softwareArchitect: "Software Architect",
+    developer: "Developer",
+    all: [
+        "Super Admin",
+        "Operation Management",
+        "Resource Management",
+        "Project Manager",
+        "Software Architect",
+        "Developer"
+    ]
 }
 module.exports = userRoles
